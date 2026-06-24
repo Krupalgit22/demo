ee6b03af300a3b082811353f2cd20e5de7454c1c
Krupalgit22 <krupalatshivraecom@gmail.com>
2026-06-24 11:41:23 +0530

    js , new pages updates

--- js/custom.js
+++ js/custom.js
@@ -304,4 +304,21 @@
     });
   }
 
-})(jQuery);
+})
+// Disable right click
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Disable inspect shortcuts
+document.addEventListener('keydown', function (e) {
+    if (
+        e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
+        (e.ctrlKey && e.key.toLowerCase() === 'u')
+    ) {
+        e.preventDefault();
+        return false;
+    }
+});
+(jQuery);
