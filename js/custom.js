8881e7a911cca788011962b2d57f06652f063b1e
Krupalgit22 <krupalatshivraecom@gmail.com>
2026-06-30 15:52:54 +0530

    Updated multiple pages and services

--- js/custom.js
+++ js/custom.js
@@ -305,20 +305,20 @@
   }
 
 })(jQuery);
+///* updated 
+// // Disable right click
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
 
-// Disable right click
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-});
-
-// Disable inspect shortcuts
-document.addEventListener('keydown', function (e) {
-    if (
-        e.key === 'F12' ||
-        (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
-        (e.ctrlKey && e.key.toLowerCase() === 'u')
-    ) {
-        e.preventDefault();
-        return false;
-    }
-});
+// // Disable inspect shortcuts
+// document.addEventListener('keydown', function (e) {
+//     if (
+//         e.key === 'F12' ||
+//         (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
+//         (e.ctrlKey && e.key.toLowerCase() === 'u')
+//     ) {
+//         e.preventDefault();
+//         return false;
+//     }
+// });
